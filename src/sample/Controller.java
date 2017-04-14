8c5c4ce0c210be9bcfd484c68487053d44af7550
Dominic Lehrmoser <dlehrmos@stud.hs-heilbronn.de>
2017-04-14 22:32:01 +0200

    Implemented board scale slider, fixed Life Cycle initializing bug. Implemented gamerules (but quick&dirty with poor performance).

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,12 +4,14 @@ import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
 import javafx.scene.image.PixelWriter;
-import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 public class Controller {
-    private final int SCALE = 100;
+    private int scale = 100;
 
     private LifeCycler lifeCycler;
 
@@ -17,10 +19,13 @@ public class Controller {
     private Canvas board;
 
     @FXML
-    private Slider speedSlider;
+    public Slider speedSlider;
 
     @FXML
     private Button startButton;
+
+    @FXML
+    private Slider scaleSlider;
 
     private PixelWriter pixelWriter;
 
@@ -31,22 +36,24 @@ public class Controller {
             lifeCycler = null;
         } else {
             pixelWriter = board.getGraphicsContext2D().getPixelWriter();
-            boolean[][] pixels = new boolean[1000 / SCALE][1000 / SCALE];
+            boolean[][] pixels = new boolean[1000 / scale][1000 / scale];
 
-            WritableImage image = new WritableImage(1000,1000);
-            board.snapshot(null,image);
+            Image image = board.snapshot(null,null);
+            PixelReader reader = image.getPixelReader();
 
-            for(int x = 0; x<1000; x+= SCALE){
-                for(int y = 0; y<1000; y+= SCALE)
-                    pixels[x/ SCALE][y/ SCALE] = image.getPixelReader().getColor(x,y) == Color.BLACK;
+            for(int x = 0; x<1000; x+= scale){
+                for(int y = 0; y<1000; y+= scale)
+                    pixels[x / scale][y / scale] = reader.getColor(x, y).equals(Color.BLACK);
             }
             updateBoard(pixels);
+            lifeCycler = new LifeCycler(pixels, this);
+            lifeCycler.start();
         }
     }
 
 
     public void updateBoard(boolean[][] pixels){
-        if (SCALE == 1) {
+        if (scale == 1) {
             for (int x = 0; x < 1000; x ++)
                 for (int y = 0; y < 1000; y ++)
                     pixelWriter.setColor(x, y, (pixels[x][y] ? Color.BLACK : Color.WHITE));
@@ -54,12 +61,46 @@ public class Controller {
             board.getGraphicsContext2D().setFill(Color.WHITE);
             board.getGraphicsContext2D().fillRect(0,0,1000,1000);
             board.getGraphicsContext2D().setFill(Color.BLACK);
-            for (int x = 0; x < 1000; x += SCALE) {
-                for (int y = 0; y < 1000; y += SCALE)
-                    if(pixels[x/ SCALE][y/ SCALE])
-                        board.getGraphicsContext2D().fillRect(x, y, SCALE, SCALE);
+            for (int x = 0; x < 1000; x += scale) {
+                for (int y = 0; y < 1000; y += scale)
+                    if(pixels[x/ scale][y/ scale])
+                        board.getGraphicsContext2D().fillRect(x, y, scale, scale);
             }
         }
+    }
+
+    @FXML
+    void mouseMoved(MouseEvent event) {
+        if(lifeCycler == null) {
+            if (scale == 1) {
+                pixelWriter = board.getGraphicsContext2D().getPixelWriter();
+                pixelWriter.setColor((int) event.getX(), (int) event.getY(), Color.BLACK);
+            } else {
+                board.getGraphicsContext2D().setFill(Color.BLACK);
+                board.getGraphicsContext2D().fillRect(Math.floor(event.getX() / scale) * scale, Math.floor(event.getY() / scale) * scale, scale, scale);
+            }
+        }
+    }
+
+    @FXML
+    void scaleChanged(MouseEvent event) {
+        int newScale = (int)scaleSlider.getValue();
+
+        while (1000 % newScale != 0)
+            newScale --;
+        if(lifeCycler != null) {
+            lifeCycler.stop();
+            lifeCycler = null;
+        }
+
+        scale = newScale;
+        updateBoard(new boolean[1000 / scale][1000 / scale]);
+    }
+
+    @FXML
+    void initialize(){
+        board.getGraphicsContext2D().setFill(Color.WHITE);
+        board.getGraphicsContext2D().fillRect(0,0,1000,1000);
     }
 }
 
--- src/sample/LifeCycler.java
+++ src/sample/LifeCycler.java
@@ -1,18 +1,80 @@
 package sample;
 
+import javafx.application.Platform;
+
 /**
  * Created by dominic on 14.04.2017.
  */
 public class LifeCycler extends Thread {
     private boolean[][] pixels;
+    private Controller controller;
 
-    public LifeCycler(boolean[][] pixels){
+    public LifeCycler(boolean[][] pixels, Controller controller){
         this.pixels = pixels;
+        this.controller = controller;
     }
 
 
     @Override
     public void run(){
+        while (true) {
+            try {
+                Thread.sleep((long) controller.speedSlider.getValue());
+            } catch (InterruptedException e) {}
 
+            gameOfLife();
+            Platform.runLater(() -> controller.updateBoard(pixels));
+        }
+    }
+
+    private void gameOfLife(){
+        boolean[][] nextIteration = pixels.clone();
+
+        //array dimensions are always proportional in this version.
+        for(int x = 0; x< pixels[0].length;x++){
+            for(int y = 0; y< pixels[0].length;y++){
+                int neighbours = 0;
+                try{
+                    if(pixels[x-1][y-1])
+                        neighbours ++;
+                } catch (IndexOutOfBoundsException e) {}
+                try{
+                    if(pixels[x-1][y])
+                        neighbours ++;
+                } catch (IndexOutOfBoundsException e) {}
+                try{
+                    if(pixels[x-1][y+1])
+                        neighbours ++;
+                } catch (IndexOutOfBoundsException e) {}
+                try{
+                    if(pixels[x][y-1])
+                        neighbours ++;
+                } catch (IndexOutOfBoundsException e) {}
+                try{
+                    if(pixels[x][y+1])
+                        neighbours ++;
+                } catch (IndexOutOfBoundsException e) {}
+                try{
+                    if(pixels[x+1][y-1])
+                        neighbours ++;
+                } catch (IndexOutOfBoundsException e) {}
+                try{
+                    if(pixels[x+1][y])
+                        neighbours ++;
+                } catch (IndexOutOfBoundsException e) {}
+                try{
+                    if(pixels[x+1][y+1])
+                        neighbours ++;
+                } catch (IndexOutOfBoundsException e) {}
+
+                if(!pixels[x][y] && neighbours == 3)
+                    nextIteration[x][y] = true;
+                else if(pixels[x][y] && (neighbours == 2 || neighbours == 3))
+                    nextIteration[x][y] = true;
+                else
+                    nextIteration[x][y] = false;
+            }
+        }
+        pixels = nextIteration;
     }
 }
