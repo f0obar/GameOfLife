1675b0a325fd654d6f0869e4db9fa190d4ba5291
Dominic Lehrmoser <dlehrmos@stud.hs-heilbronn.de>
2017-04-14 20:03:41 +0200

    implemented Canvas drawing of the board and scalability.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,20 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
 
 public class Controller {
+    private final int SCALE = 100;
+
+    private LifeCycler lifeCycler;
 
     @FXML
-    private Pane board;
+    private Canvas board;
 
     @FXML
     private Slider speedSlider;
@@ -16,9 +22,44 @@ public class Controller {
     @FXML
     private Button startButton;
 
+    private PixelWriter pixelWriter;
+
     @FXML
     void start(javafx.event.ActionEvent event) {
+        if(lifeCycler != null){
+            lifeCycler.stop();
+            lifeCycler = null;
+        } else {
+            pixelWriter = board.getGraphicsContext2D().getPixelWriter();
+            boolean[][] pixels = new boolean[1000 / SCALE][1000 / SCALE];
 
+            WritableImage image = new WritableImage(1000,1000);
+            board.snapshot(null,image);
+
+            for(int x = 0; x<1000; x+= SCALE){
+                for(int y = 0; y<1000; y+= SCALE)
+                    pixels[x/ SCALE][y/ SCALE] = image.getPixelReader().getColor(x,y) == Color.BLACK;
+            }
+            updateBoard(pixels);
+        }
+    }
+
+
+    public void updateBoard(boolean[][] pixels){
+        if (SCALE == 1) {
+            for (int x = 0; x < 1000; x ++)
+                for (int y = 0; y < 1000; y ++)
+                    pixelWriter.setColor(x, y, (pixels[x][y] ? Color.BLACK : Color.WHITE));
+        } else {
+            board.getGraphicsContext2D().setFill(Color.WHITE);
+            board.getGraphicsContext2D().fillRect(0,0,1000,1000);
+            board.getGraphicsContext2D().setFill(Color.BLACK);
+            for (int x = 0; x < 1000; x += SCALE) {
+                for (int y = 0; y < 1000; y += SCALE)
+                    if(pixels[x/ SCALE][y/ SCALE])
+                        board.getGraphicsContext2D().fillRect(x, y, SCALE, SCALE);
+            }
+        }
     }
 }
 
---
+++ src/sample/LifeCycler.java
@@ -0,0 +1,18 @@
+package sample;
+
+/**
+ * Created by dominic on 14.04.2017.
+ */
+public class LifeCycler extends Thread {
+    private boolean[][] pixels;
+
+    public LifeCycler(boolean[][] pixels){
+        this.pixels = pixels;
+    }
+
+
+    @Override
+    public void run(){
+
+    }
+}
