f5406665a3d3b0ab2b24f5967b0257dba2fc4758
Dominic Lehrmoser <dlehrmos@stud.hs-heilbronn.de>
2017-04-14 18:14:55 +0200

    added basic window layout.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Pane;
+
 public class Controller {
+
+    @FXML
+    private Pane board;
+
+    @FXML
+    private Slider speedSlider;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    void start(javafx.event.ActionEvent event) {
+
+    }
 }
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Game of Life");
+        primaryStage.setScene(new Scene(root, 1300, 1000));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
